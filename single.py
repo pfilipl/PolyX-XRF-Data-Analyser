005174672464456392192c6e34002824bf796049
Filip Baran <pfilipl@wp.pl>
2025-03-19 14:35:52 +0100

    Start configuration of Stitch UI

--- single.py
+++ single.py
@@ -29,31 +29,35 @@ class SingleWindow(QtWidgets.QWidget):
         self.pushButton_ROIsDelete.clicked.connect(self.ROIsDelete_clicked)
         self.pushButton_ROIsDeleteAll.clicked.connect(self.ROIsDeleteAll_clicked)
 
+        # Map & Spectrum
+        self.Map              = self.tab_Map
+        self.Spectrum         = self.tab_Spectrum
+
         # Map
-        self.MapPath            = self.lineEdit_MapPath.text
+        self.MapPath          = self.lineEdit_MapPath.text
 
         self.toolButton_MapPathSearch.clicked.connect(self.MapPathSearch_clicked)
 
         # Results
-        self.ResultsPath        = self.lineEdit_ResultsPath.text
+        self.ResultsPath      = self.lineEdit_ResultsPath.text
 
         self.toolButton_ResultsPathSearch.clicked.connect(self.ResultsPathSearch_clicked)
 
         # Detectors
-        self.DetectorsML        = self.pushButton_DetectorsML.isChecked()
-        self.DetectorsBe        = self.pushButton_DetectorsBe.isChecked()
-        self.DetectorsSum       = self.pushButton_DetectorsSum.isChecked()
+        self.DetectorsML      = self.pushButton_DetectorsML.isChecked()
+        self.DetectorsBe      = self.pushButton_DetectorsBe.isChecked()
+        self.DetectorsSum     = self.pushButton_DetectorsSum.isChecked()
 
         # Energy calibration
-        self.CalibrationGain    = self.doubleSpinBox_CalibrationGain.value()
-        self.CalibrationZero    = self.doubleSpinBox_CalibrationZero.value()
-        self.CalibrationNoise   = self.doubleSpinBox_CalibrationNoise.value()
-        self.CalibrationFano    = self.doubleSpinBox_CalibrationFano.value()
+        self.CalibrationGain  = self.doubleSpinBox_CalibrationGain.value()
+        self.CalibrationZero  = self.doubleSpinBox_CalibrationZero.value()
+        self.CalibrationNoise = self.doubleSpinBox_CalibrationNoise.value()
+        self.CalibrationFano  = self.doubleSpinBox_CalibrationFano.value()
 
         # Process
-        self.Progress           = self.progressBar_Progress
-        self.ReloadMap         = self.pushButton_ReloadMap
-        self.AnalyseMap         = self.pushButton_AnalyseMap
+        self.Progress         = self.progressBar_Progress
+        self.ReloadMap        = self.pushButton_ReloadMap
+        self.AnalyseMap       = self.pushButton_AnalyseMap
 
         self.pushButton_ReloadMap.clicked.connect(self.ReloadMap_clicked)
         self.pushButton_ImportConfig.clicked.connect(self.ImportConfig_clicked)
@@ -61,8 +65,8 @@ class SingleWindow(QtWidgets.QWidget):
         self.pushButton_AnalyseMap.clicked.connect(self.AnalyseMap_clicked)
 
         # Help
-        self.Help               = self.label_Help
-        self.HelpDescription    = self.label_HelpDescription
+        self.Help             = self.label_Help
+        self.HelpDescription  = self.label_HelpDescription
         
         self.Help.hide()
         self.HelpDescription.hide()
--- stitch.py
+++ stitch.py
@@ -8,6 +8,49 @@ class StitchWindow(QtWidgets.QWidget):
         super(StitchWindow, self).__init__(parent)
         uic.loadUi("stitch.ui", self)
 
+        # Top map
+        self.TopMap          = self.widget_TopMap
+        self.TopMapPath      = self.lineEdit_TopMapPath.text
+        self.TopMapOffset    = self.spinBox_TopMapOffset.value()
+
+        self.toolButton_TopMapPathSearch.clicked.connect(self.TopMapPathSearch_clicked)
+        
+        # Bottom map
+        self.BottomMap       = self.widget_BottomMap
+        self.BottomMapPath   = self.lineEdit_BottomMapPath.text
+        self.BottomMapOffset = self.spinBox_BottomMapOffset.value()
+
+        self.toolButton_BottomMapPathSearch.clicked.connect(self.BottomMapPathSearch_clicked)
+
+        # Results
+        self.ResultPath      = self.lineEdit_ResultPath.text
+
+        self.toolButton_ResultPathSearch.clicked.connect(self.ResultPathSearch_clicked)
+
+        # Process
+        self.StitchMaps      = self.pushButton_StitchMaps
+
+        self.pushButton_StitchMaps.clicked.connect(self.StitchMaps_clicked)
+
+        # Help
+        self.Help            = self.label_Help
+        self.HelpDescription = self.label_HelpDescription
+        
+        self.Help.hide()
+        self.HelpDescription.hide()
+
+    def TopMapPathSearch_clicked(self):
+        return
+
+    def BottomMapPathSearch_clicked(self):
+        return
+    
+    def ResultPathSearch_clicked(self):
+        return
+    
+    def StitchMaps_clicked(self):
+        return
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = main.MainWindow()
