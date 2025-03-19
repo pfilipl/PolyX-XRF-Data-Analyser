220fde98f12115a3010bbf1c42c58ca665a39b53
Filip Baran <pfilipl@wp.pl>
2025-03-19 16:01:18 +0100

    Start configuration of Add Roi (custom) UI

--- add_roi.py
+++ add_roi.py
@@ -14,6 +14,39 @@ class AddRoi(QtWidgets.QDialog):
         uic.loadUi("add_roi.ui", self)
         self.setWindowTitle('Add Regions of Interest (ROIs)')
 
+        # Custom
+        self.CustomName                 = self.lineEdit_CustomName.text
+
+        # - Energy
+        self.CustomEnergyLine           = self.doubleSpinBox_CustomEnergyLine.value()
+        self.CustomEnergyWidth          = self.doubleSpinBox_CustomEnergyWidth.value()
+        self.CustomEnergyChannelWidth   = self.spinBox_CustomEnergyChannelWidth.value()
+        self.CustomEnergyStart          = self.doubleSpinBox_CustomEnergyStart.value()
+        self.CustomEnergyStop           = self.doubleSpinBox_CustomEnergyStop.value()
+        self.CustomWarning              = self.label_CustomWarning
+        
+        # - Channel
+        self.CustomLine                 = self.spinBox_CustomLine.value()
+        self.CustomWidth                = self.spinBox_CustomWidth.value()
+        self.CustomStart                = self.spinBox_CustomStart.value()
+        self.CustomStop                 = self.spinBox_CustomStop.value()
+
+        # - Regions of interest (ROIs)
+        self.CustomROIs                 = self.tableView_CustomROIs
+
+        self.pushButton_CustomAdd.clicked.connect(self.CustomAdd_clicked)
+        self.pushButton_CustomDelete.clicked.connect(self.CustomDelete_clicked)
+        self.pushButton_CustomDeleteAll.clicked.connect(self.CustomDeletaALl_clicked)
+
+    def CustomAdd_clicked(self):
+        return
+
+    def CustomDelete_clicked(self):
+        return
+
+    def CustomDeletaALl_clicked(self):
+        return
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = main.MainWindow()
--- stitch.py
+++ stitch.py
@@ -9,32 +9,32 @@ class StitchWindow(QtWidgets.QWidget):
         uic.loadUi("stitch.ui", self)
 
         # Top map
-        self.TopMap          = self.widget_TopMap
-        self.TopMapPath      = self.lineEdit_TopMapPath.text
-        self.TopMapOffset    = self.spinBox_TopMapOffset.value()
+        self.TopMap             = self.widget_TopMap
+        self.TopMapPath         = self.lineEdit_TopMapPath.text
+        self.TopMapOffset       = self.spinBox_TopMapOffset.value()
 
         self.toolButton_TopMapPathSearch.clicked.connect(self.TopMapPathSearch_clicked)
         
         # Bottom map
-        self.BottomMap       = self.widget_BottomMap
-        self.BottomMapPath   = self.lineEdit_BottomMapPath.text
-        self.BottomMapOffset = self.spinBox_BottomMapOffset.value()
+        self.BottomMap          = self.widget_BottomMap
+        self.BottomMapPath      = self.lineEdit_BottomMapPath.text
+        self.BottomMapOffset    = self.spinBox_BottomMapOffset.value()
 
         self.toolButton_BottomMapPathSearch.clicked.connect(self.BottomMapPathSearch_clicked)
 
         # Results
-        self.ResultPath      = self.lineEdit_ResultPath.text
+        self.ResultPath         = self.lineEdit_ResultPath.text
 
         self.toolButton_ResultPathSearch.clicked.connect(self.ResultPathSearch_clicked)
 
         # Process
-        self.StitchMaps      = self.pushButton_StitchMaps
+        self.StitchMaps         = self.pushButton_StitchMaps
 
         self.pushButton_StitchMaps.clicked.connect(self.StitchMaps_clicked)
 
         # Help
-        self.Help            = self.label_Help
-        self.HelpDescription = self.label_HelpDescription
+        self.Help               = self.label_Help
+        self.HelpDescription    = self.label_HelpDescription
         
         self.Help.hide()
         self.HelpDescription.hide()
