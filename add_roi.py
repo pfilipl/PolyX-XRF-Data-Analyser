5f530c2a17fc0c6480111f87c1ad42fb33ab0c35
Filip Baran <pfilipl@wp.pl>
2025-03-24 17:22:31 +0100

    Connection between periodic table and ROIs table

--- add_roi.py
+++ add_roi.py
@@ -1,5 +1,5 @@
 from PyQt6 import QtWidgets, uic
-import sys
+import sys, numpy
 
 import main, periodic_table
 
@@ -27,11 +27,11 @@ class AddRoi(QtWidgets.QDialog):
         self.CustomStop                 = self.spinBox_CustomStop.value()
 
         # - Regions of interest (ROIs)
-        self.CustomROIs                 = self.tableView_CustomROIs
+        self.CustomROIs                 = self.tableWidget_CustomROIs
 
         self.pushButton_CustomAdd.clicked.connect(self.CustomAdd_clicked)
         self.pushButton_CustomDelete.clicked.connect(self.CustomDelete_clicked)
-        self.pushButton_CustomDeleteAll.clicked.connect(self.CustomDeletaALl_clicked)
+        self.pushButton_CustomDeleteAll.clicked.connect(self.CustomDeletaAll_clicked)
 
         # XRF lines
         self.XRFLines                   = self.tab_XRFLines
@@ -39,7 +39,7 @@ class AddRoi(QtWidgets.QDialog):
         self.XRFSigmaWidth              = self.doubleSpinBox_XRFSigmaWidth.value()
         self.XRFWidth                   = self.spinBox_XRFWidth.value()
 
-        # self.XRFLines.setEnabled(True)
+        self.XRFLines.setEnabled(True)
 
         # - Periodic tables
         self.Kalpha                     = self.tab_Kalpha
@@ -48,7 +48,23 @@ class AddRoi(QtWidgets.QDialog):
         self.Lbeta                      = self.tab_Lbeta
         self.M                          = self.tab_M
 
-        # self.Kalpha.setRange(20, 100)
+        self.KalphaChecked              = numpy.ones(119, numpy.bool_) * False
+        self.KbetaChecked               = numpy.ones(119, numpy.bool_) * False
+        self.LalphaChecked              = numpy.ones(119, numpy.bool_) * False
+        self.LbetaChecked               = numpy.ones(119, numpy.bool_) * False
+        self.MChecked                   = numpy.ones(119, numpy.bool_) * False
+
+        self.Kalpha.setLine("Ka")
+        self.Kbeta.setLine("Kb")
+        self.Lalpha.setLine("La")
+        self.Lbeta.setLine("Lb")
+        self.M.setLine("M")
+
+        self.Kalpha.setRange(20, 100)
+        self.Kbeta.setRange(20, 100)
+        self.Lalpha.setRange(20, 100)
+        self.Lbeta.setRange(20, 100)
+        self.M.setRange(20, 100)
 
         # Button box
         self.ButtonBox                  = self.buttonBox
@@ -59,14 +75,25 @@ class AddRoi(QtWidgets.QDialog):
         return
 
     def CustomDelete_clicked(self):
-        return
+        self.CustomROIs.removeRow(self.CustomROIs.currentRow())
 
-    def CustomDeletaALl_clicked(self):
-        return
+    def CustomDeletaAll_clicked(self):
+        self.CustomROIs.setCurrentCell(1, 1)
+        while self.CustomROIs.rowCount() > 0:
+            self.CustomROIs.removeRow(self.CustomROIs.currentRow())
+        self.Kalpha.resetElementsChecked()
+        self.Kbeta.resetElementsChecked()
+        self.Lalpha.resetElementsChecked()
+        self.Lbeta.resetElementsChecked()
+        self.M.resetElementsChecked()
     
     def ButtonBox_clicked(self, button):
         if button.text() == "Reset":
-            print("Reset")
+            self.Kalpha.resetElementsChecked()
+            self.Kbeta.resetElementsChecked()
+            self.Lalpha.resetElementsChecked()
+            self.Lbeta.resetElementsChecked()
+            self.M.resetElementsChecked()
         elif button.text() == "Discard":
             print("Discard")
             self.close()
--- periodic_table.py
+++ periodic_table.py
@@ -1,19 +1,47 @@
 from PyQt6 import QtWidgets, uic
-import sys, xraylib
+import sys, xraylib, numpy
 
 import main
 
-# I wish I could make it by this (XRF parameters when button is hover)
+# I wish I could make it like this (XRF parameters when button is hover)
 class HoverableButton(QtWidgets.QPushButton):
     def __init__(self, parent = None):
         super(HoverableButton, self).__init__(parent)
-#         self.Entered    = False
-#     def enterEvent(self, event):
-#         self.Entered    = True
-#         print("Entered")
-#     def leaveEvent(self, event):
-#         self.Entered    = False
-#         print("Leaved")
+
+    def enterEvent(self, event):
+        self.parent().findChild(QtWidgets.QLabel, "label_ElementSymbol").setText(self.text())
+
+        # self.ElementSymbol.setText(xraylib.AtomicNumberToSymbol(Z))
+        # # self.ElementName    .setText(xraylib.(Z))
+        
+        # try: Ke = xraylib.EdgeEnergy(Z, xraylib.K_SHELL)
+        # except: Ke = float("NaN")
+        # self.ElementKedge.setText(f"{Ke:.3f}")
+        # try: Le = xraylib.EdgeEnergy(Z, xraylib.L1_SHELL)
+        # except: Le = float("NaN")
+        # self.ElementLedge.setText(f"{Le:.3f}")
+        # try: Me = xraylib.EdgeEnergy(Z, xraylib.M1_SHELL)
+        # except: Me = float("NaN")
+        # self.ElementMedge.setText(f"{Me:.3f}")
+
+        # try: Ka = xraylib.LineEnergy(Z, xraylib.KA_LINE)
+        # except: Ka = float("NaN")
+        # self.ElementKalpha.setText(f"{Ka:.3f}")
+        # try: Kb = xraylib.LineEnergy(Z, xraylib.KB_LINE)
+        # except: Kb = float("NaN")
+        # self.ElementKbeta.setText(f"{Kb:.3f}")
+        # try: La = xraylib.LineEnergy(Z, xraylib.LA_LINE)
+        # except: La = float("NaN")
+        # self.ElementLalpha.setText(f"{La:.3f}")
+        # try: Lb = xraylib.LineEnergy(Z, xraylib.LB_LINE)
+        # except: Lb = float("NaN")
+        # self.ElementLbeta.setText(f"{Lb:.3f}")
+        # try: Ma = xraylib.LineEnergy(Z, xraylib.MA1_LINE)
+        # except: Ma = float("NaN")
+        # self.ElementM.setText(f"{Ma:.3f}")
+
+    def leaveEvent(self, event):
+        return
 
 class PeriodicTable(QtWidgets.QWidget):
     def __init__(self, parent = None):
@@ -34,65 +62,95 @@ class PeriodicTable(QtWidgets.QWidget):
 
         # Elements
         self.Elements           = []
-        self.ElementsChecked    = []
+        self.ElementsChecked    = numpy.ones(119, numpy.bool_) * False
 
         for Z in range(1, 119):
             exec(f"self.Elements.append(self.pushButton_Element{Z})")
-            self.ElementsChecked.append(self.Elements[Z - 1].isChecked())
-            self.Elements[Z - 1].clicked.connect(lambda checked, Z = Z: self.Element_clicked(Z))
+            self.Elements[Z - 1].clicked.connect(lambda checked, Z = Z: self.Element_clicked(checked, Z))
             self.Elements[Z - 1].pressed.connect(lambda Z = Z: self.Element_pressed(Z))
             self.Elements[Z - 1].released.connect(lambda Z = Z: self.Element_released(Z))
 
-    def Element_clicked(self, Z):
-        self.ElementsChecked[Z - 1] = self.Elements[Z - 1].isChecked()
+        # Other
+        self.line               = None
+        self.roi                = []
+        self.calib              = None
+        self.sigma              = None
 
-    def Element_pressed(self, Z):
-        self.ElementSymbol.setText(xraylib.AtomicNumberToSymbol(Z))
-        # self.ElementName    .setText(xraylib.(Z))
-        
-        try: Ke = xraylib.EdgeEnergy(Z, xraylib.K_SHELL)
-        except: Ke = float("NaN")
-        self.ElementKedge.setText(f"{Ke:.4f}")
-        try: Le = xraylib.EdgeEnergy(Z, xraylib.L1_SHELL)
-        except: Le = float("NaN")
-        self.ElementLedge.setText(f"{Le:.4f}")
-        try: Me = xraylib.EdgeEnergy(Z, xraylib.M1_SHELL)
-        except: Me = float("NaN")
-        self.ElementMedge.setText(f"{Me:.4f}")
+    def setLine(self, line):
+        self.line = line
 
-        try: Ka = xraylib.LineEnergy(Z, xraylib.KA_LINE)
-        except: Ka = float("NaN")
-        self.ElementKalpha.setText(f"{Ka:.4f}")
-        try: Kb = xraylib.LineEnergy(Z, xraylib.KB_LINE)
-        except: Kb = float("NaN")
-        self.ElementKbeta.setText(f"{Kb:.4f}")
-        try: La = xraylib.LineEnergy(Z, xraylib.LA_LINE)
-        except: La = float("NaN")
-        self.ElementLalpha.setText(f"{La:.4f}")
-        try: Lb = xraylib.LineEnergy(Z, xraylib.LB_LINE)
-        except: Lb = float("NaN")
-        self.ElementLbeta.setText(f"{Lb:.4f}")
-        try: Ma = xraylib.LineEnergy(Z, xraylib.MA1_LINE)
-        except: Ma = float("NaN")
-        self.ElementM.setText(f"{Ma:.4f}")
+    def getElementsChecked(self):
+        return self.ElementsChecked
+    
+    def setElementsChecked(self, elementsChecked):
+        self.ElementsChecked = elementsChecked
+        for Z in range(1, 119):
+            self.Elements[Z - 1].setChecked(self.ElementsChecked[Z - 1])
 
-    def Element_released(self, Z):
-        self.ElementSymbol.setText("")
-
-        self.ElementKedge.setText("")
-        self.ElementLedge.setText("")
-        self.ElementMedge.setText("")
-        
-        self.ElementKalpha.setText("")
-        self.ElementKbeta.setText("")
-        self.ElementLalpha.setText("")
-        self.ElementLbeta.setText("")
-        self.ElementM.setText("")
+    def resetElementsChecked(self):
+        for Z in range(1, 119):
+            self.Elements[Z - 1].setChecked(False)
+            self.ElementsChecked[Z - 1] = False
 
     def setRange(self, Z_start, Z_stop):
         for Z in range(1, 119):
             if Z >= Z_start and Z <= Z_stop:
                 self.Elements[Z - 1].setEnabled(True)
+
+    def Element_clicked(self, checked, Z):
+        self.ElementsChecked[Z - 1] = self.Elements[Z - 1].isChecked()
+        if checked:
+            ROIs = self.parent().parent().parent().parent().findChild(QtWidgets.QTableWidget, "tableWidget_CustomROIs")
+            ROIs.insertRow(ROIs.currentRow() + 1)
+            ROIs.setItem(ROIs.currentRow() + 1, 0, QtWidgets.QTableWidgetItem(f"{self.Elements[Z - 1].text()}-{self.line}"))
+            ROIs.setItem(ROIs.currentRow() + 1, 1, QtWidgets.QTableWidgetItem(str(1000)))
+            ROIs.setItem(ROIs.currentRow() + 1, 2, QtWidgets.QTableWidgetItem(str(1500)))
+            ROIs.setItem(ROIs.currentRow() + 1, 3, QtWidgets.QTableWidgetItem(str(1.23)))
+        else:
+            print("BEEEEEEE")
+
+    # def Element_pressed(self, Z):
+    #     self.ElementSymbol.setText(xraylib.AtomicNumberToSymbol(Z))
+    #     # self.ElementName    .setText(xraylib.(Z))
+        
+    #     try: Ke = xraylib.EdgeEnergy(Z, xraylib.K_SHELL)
+    #     except: Ke = float("NaN")
+    #     self.ElementKedge.setText(f"{Ke:.3f}")
+    #     try: Le = xraylib.EdgeEnergy(Z, xraylib.L1_SHELL)
+    #     except: Le = float("NaN")
+    #     self.ElementLedge.setText(f"{Le:.3f}")
+    #     try: Me = xraylib.EdgeEnergy(Z, xraylib.M1_SHELL)
+    #     except: Me = float("NaN")
+    #     self.ElementMedge.setText(f"{Me:.3f}")
+
+    #     try: Ka = xraylib.LineEnergy(Z, xraylib.KA_LINE)
+    #     except: Ka = float("NaN")
+    #     self.ElementKalpha.setText(f"{Ka:.3f}")
+    #     try: Kb = xraylib.LineEnergy(Z, xraylib.KB_LINE)
+    #     except: Kb = float("NaN")
+    #     self.ElementKbeta.setText(f"{Kb:.3f}")
+    #     try: La = xraylib.LineEnergy(Z, xraylib.LA_LINE)
+    #     except: La = float("NaN")
+    #     self.ElementLalpha.setText(f"{La:.3f}")
+    #     try: Lb = xraylib.LineEnergy(Z, xraylib.LB_LINE)
+    #     except: Lb = float("NaN")
+    #     self.ElementLbeta.setText(f"{Lb:.3f}")
+    #     try: Ma = xraylib.LineEnergy(Z, xraylib.MA1_LINE)
+    #     except: Ma = float("NaN")
+    #     self.ElementM.setText(f"{Ma:.3f}")
+
+    # def Element_released(self, Z):
+    #     self.ElementSymbol.setText("")
+
+    #     self.ElementKedge.setText("")
+    #     self.ElementLedge.setText("")
+    #     self.ElementMedge.setText("")
+        
+    #     self.ElementKalpha.setText("")
+    #     self.ElementKbeta.setText("")
+    #     self.ElementLalpha.setText("")
+    #     self.ElementLbeta.setText("")
+    #     self.ElementM.setText("")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
